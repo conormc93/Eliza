55747ee1e5ab132de068c5bf0b9b81f7cfdd5a60
g00291461 <g00291461@gmit.ie>
2017-11-22 10:16:12 +0000

    Changed Match String

--- Eliza.go
+++ Eliza.go
@@ -9,26 +9,21 @@ import (
 	
 )
 
-func userinputhandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w ,elizaResponse(r.URL.Query().Get("value")))
-	
-}
+func elizaResponse(w http.ResponseWriter, r *http.Request) {
 
-func elizaResponse(input string )string {
+	input := r.URL.Query().Get("value")
 
 	if matched, _ := regexp.MatchString(`(?i).*\b[Ff]ather\b.*`, input); matched{
-		return "\nWhy dont you tell me more about your father?"
+		responses := []string {
+			"\nI’m not sure what you’re trying to say. Could you explain it to me?",
+			"\nHow does that make you feel?",
+			"\nWhy do you say that?",
+			"\nAre you sure",
+		}
+		randIndex := rand.Intn(len(responses))
+		fmt.Fprintf(w, responses[randIndex])
+		return
 	}
-
-	responses := []string {
-		"\nI’m not sure what you’re trying to say. Could you explain it to me?",
-		"\nHow does that make you feel?",
-		"\nWhy do you say that?",
-		"\nAre you sure",
-	}
-
-	randIndex := rand.Intn(len(responses))
-	return responses[randIndex]
 }
 
 func main() {
@@ -37,6 +32,6 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
 
-	http.HandleFunc("/user-input", userinputhandler)
+	http.HandleFunc("/elizaResponse", elizaResponse)
 	http.ListenAndServe(":8080", nil)
 }

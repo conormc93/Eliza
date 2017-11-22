5b8b17db419d884db1a9e0e4fb7bf6321d0f7b53
g00291461 <g00291461@gmit.ie>
2017-11-22 13:03:40 +0000

    Scroll fixed. Padding fixed. Css updated.

--- Eliza.go
+++ Eliza.go
@@ -13,17 +13,50 @@ func elizaResponse(w http.ResponseWriter, r *http.Request) {
 
 	input := r.URL.Query().Get("value")
 
-	if matched, _ := regexp.MatchString(`(?i).*\b[Ff]ather\b.*`, input); matched{
+	if matched, _ := regexp.MatchString(`(?i).*\b[Hh]ello|[Hh]ey|[Hh]i.*`, input); matched{
 		responses := []string {
-			"\nI’m not sure what you’re trying to say. Could you explain it to me?",
-			"\nHow does that make you feel?",
-			"\nWhy do you say that?",
-			"\nAre you sure",
+			"Hello. Nice to meet you.",
+			"Hi there.",
+			"What's up. What do you want to talk about.",
 		}
 		randIndex := rand.Intn(len(responses))
 		fmt.Fprintf(w, responses[randIndex])
 		return
 	}
+
+	if matched, _ := regexp.MatchString(`(?i).*\b[Ww]hy.*`, input); matched{
+		responses1 := []string {
+			"Why do you ask that?",
+			"I can't tell you why, it's a secret.",
+		}
+		randIndex := rand.Intn(len(responses1))
+		fmt.Fprintf(w, responses1[randIndex])
+		return
+	}
+
+	if matched, _ := regexp.MatchString(`(?i).*\b[Ww]ho.*`, input); matched{
+		responses2 := []string {
+			"Im not sure who you are on about. Tell me more about yourself though!",
+			"I dont know who that is. Tell me more about you instead!",
+		}
+		randIndex := rand.Intn(len(responses2))
+		fmt.Fprintf(w, responses2[randIndex])
+		return
+	}
+
+	if matched, _ := regexp.MatchString(`(?i).*\b[Ww]hat\b.*`, input); matched{
+		responses3 := []string {
+			"I'm not really sure to be honest.",
+		}
+		randIndex := rand.Intn(len(responses3))
+		fmt.Fprintf(w, responses3[randIndex])
+		return
+	}
+
+
+
+
+
 }
 
 func main() {
